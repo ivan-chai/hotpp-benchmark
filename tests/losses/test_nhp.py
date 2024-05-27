beb70a300d3c04bff6f6ac40696fc0a0d36738be
Ivan Karpukhin <karpuhini@yandex.ru>
2024-05-28 00:15:37 +0300

    Improve NHP test

--- tests/losses/test_nhp.py
+++ tests/losses/test_nhp.py
@@ -20,8 +20,13 @@ class RMTPPInterpolator:
         return PaddedBatch(payload, time_deltas.seq_lens)
 
 
-def inv_softplus(x):
-    return (1 - x.exp()).clip(min=1e-6).log()
+class IdentityInterpolator:
+    """Output make NHP similar to RMTPP."""
+    def __call__(self, states, time_deltas):
+        s = time_deltas.payload.shape[2]
+        states = states[-1]  # (B, L, D).
+        states = states.unsqueeze(2).repeat(1, 1, s, 1)  # (B, L, S, D)
+        return PaddedBatch(states, time_deltas.seq_lens)
 
 
 class TestRMTPPLoss(TestCase):
@@ -46,13 +51,30 @@ class TestRMTPPLoss(TestCase):
 
         # Compare sampling.
         sample_gt = rmtpp.predict_means(predictions).squeeze(2).flatten()
-        sample = nhp.predict_next(x, states).payload["timestamps"].flatten()
+        sample = nhp.predict_next(PaddedBatch(predictions, lengths), states).payload["timestamps"].flatten()
         mean = torch.cat([sample_gt, sample]).mean()
         sample_gt -= mean
         sample -= mean
         norm = max(torch.linalg.norm(sample).item(), torch.linalg.norm(sample_gt).item())
         delta = torch.linalg.norm(sample - sample_gt).item() / norm
-        self.assertTrue(delta < 0.2)
+        self.assertTrue(delta < 0.25)
+
+    def test_labels_prediction(self):
+        nc = 5
+        nhp = NHPLoss(num_classes=nc, max_delta=5, likelihood_sample_size=1000, expectation_steps=1000)
+        nhp.interpolator = IdentityInterpolator()
+
+        b, l = 5, 7
+        lengths = torch.full([b], l)
+        predictions = torch.rand(b, l, nc)
+        for i in range(b):
+            for j in range(l):
+                predictions[i, j, (i + j) % nc] = 5
+        sample_gt = (torch.arange(b)[:, None] + torch.arange(l)[None]) % nc
+        states = predictions[None]  # (N, B, L, D).
+        sample = nhp.predict_next(PaddedBatch(predictions, lengths), states).payload["labels"]  # (b, l).
+        self.assertEqual(sample.tolist(), sample_gt.tolist())
+
 
 
 if __name__ == "__main__":
