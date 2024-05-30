b063f296eb3424cc233edf248cd19c7c423d9d89
Ivan Karpukhin <karpuhini@yandex.ru>
2024-05-30 15:06:37 +0300

    Update AgePred and Retweet configs and results

--- experiments/retweet/run-all.sh
+++ experiments/retweet/run-all.sh
@@ -1,4 +1,3 @@
 for method in next_item next_k rmtpp rmtpp_k nhp; do
     python3 -m esp_horizon.train --config-dir configs --config-name ${method}
-    python3 -m esp_horizon.eval_downstream --config-dir configs --config-name downstream +model_config=${method} ++num_workers=8
 done
