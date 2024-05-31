294af65b4f84d5665fdf4a3446f507bd03e34d32
Ivan Karpukhin <karpuhini@yandex.ru>
2024-05-31 18:13:31 +0300

    Update ablations, RMTPP gradient scale, metric parameters and results

---
+++ experiments/amazon/results/ablation-otd-map/run.sh
@@ -0,0 +1,6 @@
+for method in next_item rmtpp next_k rmtpp_k nhp; do
+    for horizon in 0.1 0.2 0.5 1 2 3 4 6 8 10; do
+        otd_c=$(echo "scale=2; ${horizon} / 2" | bc -l)
+        python3 -m esp_horizon.evaluate --config-dir configs --config-name $method ++model_path=checkpoints/${method}.ckpt ++name=${method}_h_${horizon} ++metric.map_thresholds="[$horizon]" ++metric.otd_insert_cost="$otd_c" ++metric.otd_delete_cost="$otd_c"
+    done
+done
