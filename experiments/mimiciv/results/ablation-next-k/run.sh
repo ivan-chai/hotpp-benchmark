e45a88c6055757ef611484bca644ff84efe92a90
Ivan Karpukhin <karpuhini@yandex.ru>
2024-06-05 13:48:04 +0300

    More MIMIC-IV results

---
+++ experiments/mimiciv/results/ablation-next-k/run.sh
@@ -0,0 +1,9 @@
+for method in next_k rmtpp_k; do
+    for k in 1 2 3 4; do
+        python3 -m esp_horizon.train --config-dir configs --config-name $method ++max_predictions=$k ++name=${method}_$k '~metric.otd_steps'
+    done
+
+    for k in 5 8 12 16 24; do
+        python3 -m esp_horizon.train --config-dir configs --config-name $method ++max_predictions=$k ++name=${method}_$k
+    done
+done
