0904f7120e2ee581e7daf233e1b921526668a316
Ivan Karpukhin <karpuhini@yandex.ru>
2024-06-06 02:27:57 +0300

    Add ablation results

--- experiments/amazon/results/ablation-num-predictions/run.sh
+++ experiments/amazon/results/ablation-num-predictions/run.sh
@@ -1,10 +1,14 @@
 for maxpreds in 1 2 4; do
     python3 -m esp_horizon.evaluate --config-dir configs --config-name next_item ++max_predictions=$maxpreds ++name=next_item_pred_$maxpreds ++model_path=checkpoints/next_item.ckpt '~metric.otd_steps'
     python3 -m esp_horizon.evaluate --config-dir configs --config-name rmtpp ++max_predictions=$maxpreds ++name=rmtpp_pred_$maxpreds ++model_path=checkpoints/rmtpp.ckpt '~metric.otd_steps'
-    python3 -m esp_horizon.evaluate --config-dir configs --config-name nhp ++max_predictions=$maxpreds ++name=nhp_pred_$maxpreds ++model_path=checkpoints/nhp.ckpt '~metric.otd_steps'
 done
-for maxpreds in 5 8 12 16 24 32; do
+for maxpreds in 5 8 12 16; do
     python3 -m esp_horizon.evaluate --config-dir configs --config-name next_item ++max_predictions=$maxpreds ++name=next_item_pred_$maxpreds ++model_path=checkpoints/next_item.ckpt
     python3 -m esp_horizon.evaluate --config-dir configs --config-name rmtpp ++max_predictions=$maxpreds ++name=rmtpp_pred_$maxpreds ++model_path=checkpoints/rmtpp.ckpt
+done
+for maxpreds in 1 2 4; do
+    python3 -m esp_horizon.evaluate --config-dir configs --config-name nhp ++max_predictions=$maxpreds ++name=nhp_pred_$maxpreds ++model_path=checkpoints/nhp.ckpt '~metric.otd_steps'
+done
+for maxpreds in 5 8 12 16; do
     python3 -m esp_horizon.evaluate --config-dir configs --config-name nhp ++max_predictions=$maxpreds ++name=nhp_pred_$maxpreds ++model_path=checkpoints/nhp.ckpt
 done
---
+++ experiments/mimiciv/results/ablation-otd-map/run.sh
@@ -0,0 +1,6 @@
+for method in next_item rmtpp next_k rmtpp_k; do
+    for horizon in 1 2 4 8 16 22 28; do
+        otd_c=$(echo "scale=2; ${horizon} / 2" | bc -l)
+        python3 -m esp_horizon.evaluate --config-dir configs --config-name $method ++model_path=checkpoints/${method}.ckpt ++name=${method}_h_${horizon} ++metric.map_thresholds="[$horizon]" ++metric.otd_insert_cost="$otd_c" ++metric.otd_delete_cost="$otd_c"
+    done
+done
