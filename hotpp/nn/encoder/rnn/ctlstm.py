8a473c55ae1fa1ebf351725d337d9bc4d20827db
Ivan Karpukhin <karpuhini@yandex.ru>
2024-07-31 13:40:04 +0300

    Speedup CTLSTM

--- hotpp/nn/encoder/rnn/ctlstm.py
+++ hotpp/nn/encoder/rnn/ctlstm.py
@@ -1,50 +1,74 @@
+import math
 import torch
 import torch.nn.functional as F
 from torch import Tensor
-from typing import Tuple, Optional
+from typing import List, Tuple, Optional
 
 from hotpp.data import PaddedBatch
 
 
-@torch.jit.script
-def cont_time_lstm(x, time_deltas, states, weight, bias):
-    """Apply continuos-time LSTM using a simple PyTorch loop.
+class ContTimeLSTMCell(torch.jit.ScriptModule):
+    # 7 states: i, f, ie, fe, o, z, d.
+    def __init__(self, input_size, hidden_size):
+        super().__init__()
+        self.input_size = input_size
+        self.hidden_size = hidden_size
+        self.projection = torch.nn.Linear(input_size, 7 * hidden_size, bias=False)
+        self.weight = torch.nn.Parameter(torch.empty(hidden_size, 7 * hidden_size))  # (D, 7D).
+        self.bias = torch.nn.Parameter(torch.empty(7 * hidden_size))  # (7D).
+        self.reset_parameters()
 
-    Args:
-        x: Batch with shape (B, L, I).
-        time_deltas: Relative timestamps with shape (B, L).
-        states: States, each with shape (B, 4D).
-        weight: Layer weight matrix with shape (7D, I + D).
-        bias: Layer bias vector with shape (7D).
+    def reset_parameters(self):
+        self.projection.reset_parameters()
+        stdv = 1.0 / math.sqrt(self.hidden_size) if self.hidden_size > 0 else 0
+        torch.nn.init.uniform_(self.weight, -stdv, stdv)
+        torch.nn.init.uniform_(self.bias, -stdv, stdv)
 
-    Returns:
-        Model outputs with shape (B, L, D) and states with shape (B, L, 4D).
-    """
-    b, l, x_dim = x.shape
-    assert states.shape[1] % 4 == 0
-    cs_state, ce_state, decay, o_gate = states.chunk(4, 1)
-    h_dim = o_gate.shape[1]
-    x_proj = F.linear(x, weight[:, :x_dim])  # (B, L, 7D).
-    h_weight = weight[:, x_dim:]  # (7D, D).
+    def preprocess(self, input):
+        """Prepare input for recurrent processing.
 
-    outputs = []
-    output_states = []
-    for step in range(l):
-        c = ce_state + (cs_state - ce_state) * (-decay * time_deltas[:, step, None]).exp()  # (B, D).
+        Args:
+            input: Input data with shape (*, I).
+
+        Returns:
+            Projected input with shape (*, D).
+        """
+        return self.projection(input)
+
+    @torch.jit.script_method
+    def forward(
+            self,
+            input: Tensor,
+            time_deltas: Tensor,
+            state: Tuple[Tensor, Tensor, Tensor, Tensor]
+    ) -> Tuple[Tensor, Tuple[Tensor, Tensor, Tensor, Tensor]]:
+        """Make single step.
+
+        NOTE: input must be preprocessed before call to forward.
+
+        Args:
+            input: Preprocessed input tensor with shape (B, D).
+            time_deltas: Time steps with shape (B).
+            state: Tuple of 4 states, each with shape (B, D).
+
+        Returns:
+            Output and a tuple of new states, each with shape (B, D).
+        """
+        dim = self.hidden_size
+        cs_state, ce_state, decay, o_gate = state  # (B, D) each.
+        c = ce_state + (cs_state - ce_state) * (-decay * time_deltas.unsqueeze(1)).exp()  # (B, D).
         h = o_gate * torch.tanh(c)  # (B, D).
-        proj = x_proj[:, step] + F.linear(h, h_weight, bias)  # (B, 7D).
-        sigmoid_proj = torch.sigmoid(proj[:, :5 * h_dim])  # (B, 5D).
+        proj = input + torch.mm(h, self.weight) + self.bias  # (B, 7D).
+        sigmoid_proj = torch.sigmoid(proj[:, :5 * dim])  # (B, 5D).
         i_gate, f_gate, ie_gate, fe_gate, o_gate = sigmoid_proj.chunk(5, 1)  # (B, D).
-        z = torch.tanh(proj[:, 5 * h_dim:6 * h_dim])  # (B, D).
+        z = torch.tanh(proj[:, 5 * dim:6 * dim])  # (B, D).
         cs_state = f_gate * c + i_gate * z
         ce_state = fe_gate * ce_state + ie_gate * z
-        decay = torch.nn.functional.softplus(proj[:, 6 * h_dim:7 * h_dim])
-        outputs.append(h)  # (B, D).
-        output_states.append(torch.cat([cs_state, ce_state, decay, o_gate], 1))  # (B, 4D).
-    return torch.stack(outputs, 1), torch.stack(output_states, 1)  # (B, L, D), (B, L, 4D).
+        decay = torch.nn.functional.softplus(proj[:, 6 * dim:7 * dim])
+        return h, (cs_state, ce_state, decay, o_gate)
 
 
-class ContTimeLSTM(torch.nn.Module):
+class ContTimeLSTM(torch.jit.ScriptModule):
     """Continuous time LSTM from NHP method.
 
     See the original paper for details:
@@ -52,35 +76,57 @@ class ContTimeLSTM(torch.nn.Module):
     Mei, Hongyuan, and Jason M. Eisner. "The neural hawkes process: A neurally self-modulating
     multivariate point process." Advances in neural information processing systems 30 (2017).
 
-    NOTE: Our implementation is slightly different because our first time_delta is always zero.
+    NOTE: Our implementation is slightly different because our first time delta is always zero.
     This way we encode time from a previous event rather than time to a future event.
     """
     def __init__(self, input_size, hidden_size, num_layers=1):
         super().__init__()
         if num_layers != 1:
             raise NotImplementedError("Cont-LSTM with multiple layers")
-        self._hidden_size = hidden_size
-        self._num_layers = num_layers
+        self.num_layers = num_layers
+        self.cell = ContTimeLSTMCell(input_size, hidden_size)
         self.bos = torch.nn.Parameter(torch.randn(input_size))  # (D).
-        layer = torch.nn.Linear(input_size + hidden_size, 7 * hidden_size)  # i, f, ie, fe, o, z, d.
-        self.weight = layer.weight
-        self.bias = layer.bias
 
     @property
     def output_size(self):
-        return self._hidden_size
-
-    @property
-    def num_layers(self):
-        return self._num_layers
+        return self.cell.hidden_size
 
     @property
     def init_state(self):
-        bos = self.bos[None, None]  # (1, 1, D).
-        zero_state = torch.zeros(1, 4 * self._hidden_size, dtype=self.bos.dtype, device=self.bos.device)  # (1, 4D).
-        zero_dt = torch.zeros(1, 1, dtype=self.bos.dtype, device=self.bos.device)  # (1, 1).
-        _, states = cont_time_lstm(bos, zero_dt, zero_state, self.weight, self.bias)  # (1, 1, 4D).
-        return states.squeeze(1)  # (1, 4D).
+        dtype, device = self.bos.dtype, self.bos.device
+        bos = self.cell.preprocess(self.bos[None, None]).squeeze(0)  # (1, D).
+        dt = torch.zeros(1, dtype=dtype, device=device)  # (1).
+        zeros = torch.zeros(1, self.cell.hidden_size, dtype=dtype, device=device)
+        state = (zeros, zeros, zeros, zeros)
+        _, bos_state = self.cell(bos, dt, state)
+        return torch.cat(bos_state, 1)  # (1, 4D).
+
+    @torch.jit.script_method
+    def _forward_loop(self,
+                      x: Tensor,
+                      time_deltas: Tensor,
+                      state: Tuple[Tensor, Tensor, Tensor, Tensor]
+                      ) -> Tuple[Tensor, Tuple[Tensor, Tensor, Tensor, Tensor]]:
+        inputs = x.unbind(1)
+        dt = time_deltas.unbind(1)
+        outputs = torch.jit.annotate(List[Tensor], [])
+        output_cs_states = torch.jit.annotate(List[Tensor], [])
+        output_ce_states = torch.jit.annotate(List[Tensor], [])
+        output_decay_states = torch.jit.annotate(List[Tensor], [])
+        output_gate_states = torch.jit.annotate(List[Tensor], [])
+        for i in range(len(inputs)):
+            out, state = self.cell(inputs[i], dt[i], state)
+            outputs += [out]
+            output_cs_states += [state[0]]
+            output_ce_states += [state[1]]
+            output_decay_states += [state[2]]
+            output_gate_states += [state[3]]
+        return torch.stack(outputs, 1), (
+               torch.stack(output_cs_states, 1),
+               torch.stack(output_ce_states, 1),
+               torch.stack(output_decay_states, 1),
+               torch.stack(output_gate_states, 1)
+        )
 
     def forward(self, x: PaddedBatch, time_deltas: PaddedBatch,
                 states: Optional[Tensor]=None, return_full_states=False) -> Tuple[PaddedBatch, Tensor]:
@@ -97,10 +143,8 @@ class ContTimeLSTM(torch.nn.Module):
         Returns:
             Output with shape (B, L, D) and optional states tensor with shape (1, B, L, 4D).
         """
-        if self._num_layers != 1:
-            raise NotImplementedError("Multiple layers.")
         b, l = x.shape
-        s = self._hidden_size
+        dim = self.cell.hidden_size
         seq_lens, mask = x.seq_lens, x.seq_len_mask  # (B), (B, L).
         x = x.payload * mask.unsqueeze(2)  # (B, L, D).
         time_deltas = time_deltas.payload * mask  # (B, L).
@@ -108,11 +152,24 @@ class ContTimeLSTM(torch.nn.Module):
             states = self.init_state.repeat(b, 1)  # (B, 4D).
         else:
             states = states.squeeze(0)  # Remove layer dim, (B, 4D).
-        outputs, output_states = cont_time_lstm(x, time_deltas, states, self.weight, self.bias)  # (B, L, D), (B, L, 4D).
+        states = states.chunk(4, 1)  # 4 x (B, D).
+        x = self.cell.preprocess(x)  # (B, L, D).
+        outputs, output_states = self._forward_loop(x, time_deltas, states)  # (B, L, D), 4 x (B, L, D).
+        output_states = torch.cat(output_states, -1)  # (B, L, 4D).
         outputs = PaddedBatch(outputs, seq_lens)
         if not return_full_states:
             output_states = output_states.take_along_dim((seq_lens - 1).clip(min=0)[:, None, None], 1).squeeze(1)  # (B, 4D).
         return outputs, output_states[None]  # (1, B, 4D) or (1, B, L, 4D).
+
+    @torch.jit.script_method
+    def _interpolate_impl(self, states: Tensor, time_deltas: Tensor) -> Tensor:
+        # STATES: (B, L, D).
+        # time_deltas: (B, L, S).
+        # output: (B, L, S, D).
+        cs_state, ce_state, decay, o_gate = states.unsqueeze(2).chunk(4, -1)  # (B, L, 1, D).
+        c = ce_state + (cs_state - ce_state) * (-decay * time_deltas.unsqueeze(3)).exp()  # (B, L, S, D).
+        h = o_gate * torch.tanh(c)  # (B, L, S, D).
+        return h
 
     def interpolate(self, states: Tensor, time_deltas: PaddedBatch) -> PaddedBatch:
         """Compute model outputs in continuous time.
@@ -124,8 +181,5 @@ class ContTimeLSTM(torch.nn.Module):
         Returns:
             Outputs with shape (B, L, S, D).
         """
-        states = states[-1].unsqueeze(2)  # (B, L, 1, 4D).
-        cs_state, ce_state, decay, o_gate = states.chunk(4, -1)  # (B, L, 1, D).
-        c = ce_state + (cs_state - ce_state) * (-decay * time_deltas.payload.unsqueeze(3)).exp()  # (B, L, S, D).
-        h = o_gate * torch.tanh(c)  # (B, L, S, D).
+        h = self._interpolate_impl(states[-1], time_deltas.payload)  # (B, L, S, D).
         return PaddedBatch(h, time_deltas.seq_lens)
