0004a1b0f1154483efdb2b50aa8a05cbc4e5ea51
Ivan Karpukhin <karpuhini@yandex.ru>
2024-07-31 21:00:54 +0300

    Update notebooks

--- hotpp/losses/__init__.py
+++ hotpp/losses/__init__.py
@@ -1,6 +1,8 @@
+# Base losses.
 from .common import BaseLoss, TimeMAELoss, CrossEntropyLoss
 from .rmtpp import TimeRMTPPLoss
-from .nhp import NHPLoss
 
+# High-level losses.
+from .nhp import NHPLoss
 from .next_item import NextItemLoss
 from .next_k import NextKLoss
