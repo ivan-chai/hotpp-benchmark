621f8e1924b00633629464efb10ebd0f6a527de5
Ivan Karpukhin <karpuhini@yandex.ru>
2024-08-02 17:09:11 +0300

    Update statistical baselines

--- experiments/transactions-agepred/run-all.sh
+++ experiments/transactions-agepred/run-all.sh
@@ -1,4 +1,6 @@
-for method in next_item next_k rmtpp rmtpp_k nhp; do
-    python3 -m hotpp.train --config-dir configs --config-name ${method}
+for method in most_popular most_popular_otd recent_history recent_history_otd next_item next_k rmtpp rmtpp_k nhp ode; do
+    python3 -m hotpp.train_multiseed --config-dir configs --config-name ${method}
+done
+for method in next_item next_k rmtpp rmtpp_k nhp ode; do
     python3 -m hotpp.eval_downstream --config-dir configs --config-name downstream +model_config=${method} ++num_workers=8
 done
