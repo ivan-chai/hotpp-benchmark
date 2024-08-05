47fcf6fdc2ae86305e51fb3f89a601bf9d5e7c25
Ivan Karpukhin <karpuhini@yandex.ru>
2024-08-05 15:57:10 +0300

    More ablations

--- experiments/amazon/results/ablation-num-predictions/run.sh
+++ experiments/amazon/results/ablation-num-predictions/run.sh
@@ -8,7 +8,9 @@ for maxpreds in 5 8 12 16; do
 done
 for maxpreds in 1 2 4; do
     python3 -m hotpp.evaluate --config-dir configs --config-name nhp ++max_predictions=$maxpreds ++name=nhp_pred_$maxpreds ++model_path=checkpoints/nhp.ckpt '~metric.otd_steps'
+    python3 -m hotpp.evaluate --config-dir configs --config-name ode ++max_predictions=$maxpreds ++name=ode_pred_$maxpreds ++model_path=checkpoints/ode.ckpt '~metric.otd_steps'
 done
 for maxpreds in 5 8 12 16; do
     python3 -m hotpp.evaluate --config-dir configs --config-name nhp ++max_predictions=$maxpreds ++name=nhp_pred_$maxpreds ++model_path=checkpoints/nhp.ckpt
+    python3 -m hotpp.evaluate --config-dir configs --config-name ode ++max_predictions=$maxpreds ++name=ode_pred_$maxpreds ++model_path=checkpoints/ode.ckpt
 done
