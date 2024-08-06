326bf8eeb4fe8e9fe14cae7c8f544f6156eb3128
Ivan Karpukhin <karpuhini@yandex.ru>
2024-08-06 11:50:52 +0300

    Add missing ablations

--- experiments/retweet/results/ablation-num-predictions/run.sh
+++ experiments/retweet/results/ablation-num-predictions/run.sh
@@ -1,4 +1,4 @@
-for maxpreds in 5 8; do
+for maxpreds in 1 2 4 5 8; do
     python3 -m hotpp.evaluate --config-dir configs --config-name next_item ++max_predictions=$maxpreds ++name=next_item_pred_$maxpreds ++model_path=checkpoints/next_item.ckpt '~metric.otd_steps'
     python3 -m hotpp.evaluate --config-dir configs --config-name rmtpp ++max_predictions=$maxpreds ++name=rmtpp_pred_$maxpreds ++model_path=checkpoints/rmtpp.ckpt '~metric.otd_steps'
 done
