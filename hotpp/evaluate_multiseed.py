305024be9470f86b8e958d0f989ecd3486ed0af1
Ivan Karpukhin <karpuhini@yandex.ru>
2024-08-29 14:51:23 +0300

    Improve config handling in scripts

--- hotpp/evaluate_multiseed.py
+++ hotpp/evaluate_multiseed.py
@@ -12,7 +12,7 @@ logger = logging.getLogger(__name__)
 @hydra.main(version_base="1.2", config_path=None)
 def main(conf):
     OmegaConf.set_struct(conf, False)
-    conf.pop("logger")
+    conf.pop("logger", None)
     conf["test_only"] = True
     train_multiseed(conf)
 
