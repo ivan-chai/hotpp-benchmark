e7a82c62d7a5f6f6589b0f1a96a904d6e76cf291
Ivan Karpukhin <karpuhini@yandex.ru>
2024-09-30 11:53:32 +0300

    More sampling ablation results

--- experiments/stackoverflow/results/ablation-sampling/run.sh
+++ experiments/stackoverflow/results/ablation-sampling/run.sh
@@ -1,8 +1,8 @@
 for temperature in 10 2 1 0.5 0.1; do
-    for method in detection detection_hybrid; do
+    for method in detection detection_hybrid rmtpp; do
         python3 -m hotpp.evaluate --config-dir configs --config-name ${method}_sample name=${method}_t_${temperature} model_path=checkpoints/${method}.ckpt +module.loss.next_item_loss.temperature="${temperature}"
     done
-    for method in next_item rmtpp; do
+    for method in next_item; do
         python3 -m hotpp.evaluate --config-dir configs --config-name ${method} name=${method}_t_${temperature} model_path=checkpoints/${method}.ckpt +module.loss.prediction=sample +module.loss.temperature="${temperature}"
     done
     for method in nhp ode attnhp; do
