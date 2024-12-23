cb66e97ad0a681fe119ce7af6eb7a47f59bfd9f0
Ivan Karpukhin <karpuhini@yandex.ru>
2024-12-23 12:18:30 +0300

    Update and extend MBD experiments

--- experiments/transactions-mbd/scripts/make-dataset.py
+++ experiments/transactions-mbd/scripts/make-dataset.py
@@ -25,17 +25,22 @@ def join_and_convert_datasets(srcs, dst, n_partitions):
     for path in srcs:
         part = spark.read.parquet(path)
         dataset = dataset.union(part) if dataset is not None else part
+    # Compute log_amount.
     udf = F.udf(lambda x: [math.log(abs(v) + 1) for v in x], ArrayType(FloatType(), False))
     dataset = dataset.withColumn("log_amount", udf(F.col("amount")))
+    # Scale event_time by 4 days.
+    udf = F.udf(lambda x: [v / 345600 for v in x], ArrayType(FloatType(), False))
+    dataset = dataset.withColumn("timestamps", udf(F.col("event_time")))
+    # Dump.
     dataset.repartition(n_partitions, "client_id").write.mode("overwrite").parquet(dst)
 
 
 def load_transactions(root, cache_dir):
     # Download and extract.
-    hf_hub_download(repo_id="ai-lab/MBD", filename="ptls.tar.gz", repo_type="dataset", local_dir=cache_dir)
-    archive = os.path.join(cache_dir, "ptls.tar.gz")
-    with tarfile.open(archive, "r:gz") as fp:
-        fp.extractall(path=root)
+    #hf_hub_download(repo_id="ai-lab/MBD", filename="ptls.tar.gz", repo_type="dataset", local_dir=cache_dir)
+    #archive = os.path.join(cache_dir, "ptls.tar.gz")
+    #with tarfile.open(archive, "r:gz") as fp:
+    #    fp.extractall(path=root)
     # Merge folds.
     join_and_convert_datasets([os.path.join(root, "ptls", "trx", f"fold={fold}") for fold in [0, 1, 2, 3]],
                               os.path.join(root, "train.parquet"),
