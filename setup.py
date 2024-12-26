0f8103c42a2d6fd0c03dcce2be2f554f76f5cec8
Ivan Karpukhin <karpuhini@yandex.ru>
2024-12-26 12:05:43 +0300

    Update version

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ if not os.environ.get("HOTPP_PUBLISH", False):
 
 setuptools.setup(
     name="hotpp-benchmark",
-    version="0.0.2",
+    version="0.1.0",
     author="Ivan Karpukhin",
     author_email="karpuhini@yandex.ru",
     description="Evaluate generative event sequence models on the long horizon prediction task.",
