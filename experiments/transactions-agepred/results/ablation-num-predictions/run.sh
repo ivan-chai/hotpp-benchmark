0741d100b45288b710577c6dda3e33d0a02a1ea7
Ivan Karpukhin <karpuhini@yandex.ru>
2025-02-24 10:24:38 +0300

    Add ablations for next_item_transformer and diffusion

--- experiments/transactions-agepred/results/ablation-num-predictions/run.sh
+++ experiments/transactions-agepred/results/ablation-num-predictions/run.sh
@@ -1,10 +1,10 @@
 for maxpreds in 1 2 4; do
-    for method in next_item next_item_transformer rmtpp nhp ode detection detection_hybrid diffusion; do
+    for method in next_item next_item_transformer rmtpp nhp ode detection detection_hybrid diffusion_gru; do
         python3 -m hotpp.evaluate --config-dir configs --config-name ${method} ++max_predictions=$maxpreds ++name=${method}_pred_$maxpreds ++model_path=checkpoints/${method}.ckpt '~metric.otd_steps'
     done
 done
 for maxpreds in 5 8 12 16 24 32; do
-    for method in next_item next_item_transformer rmtpp nhp ode detection detection_hybrid diffusion; do
+    for method in next_item next_item_transformer rmtpp nhp ode detection detection_hybrid diffusion_gru; do
         python3 -m hotpp.evaluate --config-dir configs --config-name ${method} ++max_predictions=$maxpreds ++name=${method}_pred_$maxpreds ++model_path=checkpoints/${method}.ckpt
     done
 done
--- experiments/transactions-agepred/results/ablation-otd-map/run.sh
+++ experiments/transactions-agepred/results/ablation-otd-map/run.sh
@@ -1,6 +1,6 @@
 for method in next_item next_item_transformer rmtpp next_k rmtpp_k nhp ode detection detection_hybrid diffusion_gru; do
     for horizon in 0.1 0.2 0.5 1 2 3 4 5 6 7; do
         otd_c=$(echo "scale=2; ${horizon} / 2" | bc -l)
-        python3 -m hotpp.evaluate --config-dir configs --config-name $method ++model_path=checkpoints/${method}.ckpt ++name=${method}_h_${horizon} ++metric.map_thresholds="[$horizon]" ++metric.otd_insert_cost="$otd_c" ++metric.otd_delete_cost="$otd_c"
+        python3 -m hotpp.evaluate --config-dir configs --config-name $method ++model_path=checkpoints/${method}.ckpt ++name=${method}_h_${horizon} ++metric.map_deltas="[$horizon]" ++metric.otd_insert_cost="$otd_c" ++metric.otd_delete_cost="$otd_c"
     done
 done
