fb4dd049daa539cdedb0ad027ad4e0cdf96d761f
Ivan Karpukhin <karpuhini@yandex.ru>
2025-04-09 17:11:37 +0300

    Add longer horizon evaluation scripts

---
+++ experiments/retweet/results/longer-horizon/run.sh
@@ -0,0 +1,17 @@
+set -e
+for k in 1 2 3 4; do
+    max_predictions=$(( 32 * $k ))
+    horizon=$(( 180 * $k ))
+    otd=$(( 10 * $k ))
+    for method in next_item next_item_transformer rmtpp nhp ode; do
+        python3 -m hotpp.evaluate --config-dir configs --config-name ${method} metric.otd_steps=$otd metric.horizon=$horizon max_predictions=$max_predictions name=${method}_longer_$k model_path=checkpoints/${method}.ckpt
+    done
+done
+for k in 1 2 3 4; do
+    max_predictions=$(( 32 * $k ))
+    horizon=$(( 180 * $k ))
+    otd=$(( 10 * $k ))
+    for method in detection_hybrid diffusion_gru; do
+        python3 -m hotpp.evaluate --config-dir configs --config-name ${method} metric.otd_steps=$otd metric.horizon=$horizon max_predictions=$max_predictions name=${method}_longer_$k model_path=checkpoints/${method}.ckpt data_module.batch_size=16 +module.recurrent_steps=$k
+    done
+done
