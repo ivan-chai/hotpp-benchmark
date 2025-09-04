7ab377b23166ecc2f6db96018856a156558a6645
Ivan Karpukhin <karpuhini@yandex.ru>
2025-09-04 18:47:55 +0300

    Add MHA test

--- tests/nn/encoder/test_rope.py
+++ tests/nn/encoder/test_rope.py
@@ -1,6 +1,7 @@
 from unittest import TestCase, main
 
 import torch
+import itertools
 from torch.nn import MultiheadAttention
 
 from hotpp.nn.encoder.transformer.rope import TimeRoPEEncoding, MultiheadAttentionRoPE
@@ -59,13 +60,59 @@ class TestRoPE(TestCase):
         o3.nan_to_num_(0, 0, 0)
         w3.nan_to_num_(0, 0, 0)
         assert o2.allclose(o3, atol=1e-6) and w2.allclose(w3, atol=1e-6)
-        
+
         timestamps = timestamps * 2  # Multiply time, different result.
         with rope.cache(timestamps):
             o3, w3 = layer_rope(q, k, v, key_padding_mask=key_padding_mask, attn_mask=attn_mask, rope=rope)
         o3.nan_to_num_(0, 0, 0)
         w3.nan_to_num_(0, 0, 0)
         assert (not o2.allclose(o3, atol=1e-6)) and (not w2.allclose(w3, atol=1e-6))
+
+
+class TestMHA(TestCase):
+    def test_mha(self):
+        dim = 16
+        for batch_first, group_size in itertools.product([True, False], [1, 2, 4]):
+            kvdim = dim // group_size
+            base = MultiheadAttentionRoPE(
+                dim,  # embed_dim.
+                4,   # num_heads.
+                group_size=group_size,
+                batch_first=batch_first,
+                dropout=0
+            )
+            assert base._qkv_same_embed_dim
+            split = MultiheadAttentionRoPE(
+                dim,  # embed_dim.
+                4,   # num_heads.
+                group_size=group_size,
+                batch_first=batch_first,
+                dropout=0
+            )
+            split.load_state_dict(base.state_dict())
+            split._qkv_same_embed_dim = False
+            split.q_proj_weight = torch.nn.Parameter(split.in_proj_weight.data[:dim].clone())
+            split.k_proj_weight = torch.nn.Parameter(split.in_proj_weight.data[dim:dim + kvdim].clone())
+            split.v_proj_weight = torch.nn.Parameter(split.in_proj_weight.data[dim + kvdim:].clone())
+
+            b, l, d = 18, 7, 16
+            q = torch.randn(b, l, d)
+            k = torch.randn(b, l, d)
+            v = torch.randn(b, l, d)
+            if not batch_first:
+                q = q.transpose(0, 1)
+                k = k.transpose(0, 1)
+                v = v.transpose(0, 1)
+            y_gt, w_gt = base(q, k, v, need_weights=False)
+            self.assertTrue(w_gt is None)
+            y, w_gt = base(q, k, v, need_weights=True)
+            self.assertTrue(y.allclose(y_gt, atol=1e-6))
+            y, w = split(q, k, v, need_weights=False)
+            self.assertTrue(w is None)
+            self.assertTrue(y.allclose(y_gt, atol=1e-6))
+            y, w = split(q, k, v, need_weights=True)
+            self.assertTrue(y.allclose(y_gt, atol=1e-6))
+            self.assertTrue(w.allclose(w_gt, atol=1e-6))
 
 
 if __name__ == "__main__":
