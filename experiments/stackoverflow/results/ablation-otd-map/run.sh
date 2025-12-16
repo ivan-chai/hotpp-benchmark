074a01947f9899a628d9acc94e3934f20d59025e
Ivan Karpukhin <karpuhini@yandex.ru>
2025-12-16 15:17:26 +0300

    Update ablations

--- experiments/stackoverflow/results/ablation-otd-map/run.sh
+++ experiments/stackoverflow/results/ablation-otd-map/run.sh
@@ -1,6 +1,17 @@
-for method in next_item next_item_transformer rmtpp next_k rmtpp_k ode nhp detection detection_hybrid diffusion_gru; do
-    for horizon in 0.1 0.2 0.5 1 2 3 4 6 8 10; do
-        otd_c=$(echo "scale=2; ${horizon} / 2" | bc -l)
-        python3 -m hotpp.evaluate --config-dir configs --config-name $method ++model_path=checkpoints/${method}.ckpt ++name=${method}_h_${horizon} ++metric.map_deltas="[$horizon]" ++metric.otd_insert_cost="$otd_c" ++metric.otd_delete_cost="$otd_c"
+BASE_HORIZON=$(cat configs/default.yaml | grep ' horizon:' | sed 's/#.*//;s/.*://' | tr -d ' ')
+BASE_DELTA=$(cat configs/default.yaml | grep ' map_deltas:' | sed 's/#.*//;s/.*://' | tr -d ' []')
+BASE_COST=$(cat configs/default.yaml | grep ' otd_insert_cost:' | sed 's/#.*//;s/.*://' | tr -d ' ')
+for method in next_item diffusion_gru next_item_transformer rmtpp nhp hypro_rmtpp; do
+    if [[ $method == "hypro_rmtpp" ]]; then
+        n_seeds=1
+    else
+        n_seeds=3
+    fi
+    for scale in 0.1 0.2 0.5 1 2; do
+        otd_c=$(echo "${BASE_COST} * ${scale}" | bc -l)
+        horizon=$(echo "${BASE_HORIZON} * ${scale}" | bc -l)
+        delta="${BASE_DELTA}"
+        name=${method}_h_${horizon}_c_${otd_c}
+        python3 -m hotpp.evaluate_multiseed --config-dir configs --config-name $method ++model_path=checkpoints/${method}.ckpt name=$name metric.map_deltas="[$delta]" metric.horizon="$horizon" metric.otd_insert_cost="$otd_c" metric.otd_delete_cost="$otd_c" num_evaluation_seeds=$n_seeds
     done
 done
